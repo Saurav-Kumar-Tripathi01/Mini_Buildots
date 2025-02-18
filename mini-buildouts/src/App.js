71dcc4b322887baf17ee2ba36a8811ae5c1732d6
Saurav Kumar Tripathi <152642735+Saurav-Kumar-Tripathi01@users.noreply.github.com>
2025-02-18 09:19:24 +0000

    Final-buildouts

--- mini-buildouts/src/App.js
+++ mini-buildouts/src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
-// import Xlogin from './xlogin';
-// import Xstopwatch from './xstopwatch';
-// import Xweather from './Xweather/xweather';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Xlogin from './xlogin';
+import Xstopwatch from './xstopwatch';
+import Xweather from './Xweather/xweather';
 import XSpellCheck from './xspellcheck';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Xlogin /> */}
-      {/* <Xstopwatch /> */}
-      {/* <Xweather /> */}
-      <XSpellCheck />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/stopwatch">Stopwatch</Link>
+            </li>
+            <li>
+              <Link to="/weather">Weather</Link>
+            </li>
+            <li>
+              <Link to="/spellcheck">Spell Check</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login" component={Xlogin} />
+          <Route path="/stopwatch" component={Xstopwatch} />
+          <Route path="/weather" component={Xweather} />
+          <Route path="/spellcheck" component={XSpellCheck} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
